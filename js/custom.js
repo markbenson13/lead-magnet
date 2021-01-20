96c2e598bce6395344c6d5a1aec0f1377e20366e
Benson Opeña <bopena@Marks-MacBook-Pro.local>
2021-01-20 20:09:07 +0800

    Add animation and fix mobile responsiveness

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,15 @@
 (function($) {
 
-    $(document).on("ready", function() {
+    // console.log("hello");
+
+    $(document).ready( function() {
 
         $('.merchandise-container').slick({
+            autoplay:true,
+            autoplaySpeed: 3000,
             infinite: true,
             dots: true,
+            arrows: false,
             slidesToShow: 1,
             slidesToScroll: 1
         });
